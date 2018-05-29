dfbb0b6be8df4863911b11f6a1a06de737c2faf5
Nancho Alvarez <36212927+Nancho-Alvarez@users.noreply.github.com>
2018-05-29 17:29:15 +0200

    The rhythm patterns seem to work

--- galamian.py
+++ galamian.py
@@ -7,7 +7,7 @@ class rithmic_pattern:
         self.time_signature=time_signature
         string=string.replace('~', ' ~ ')
         string=re.sub('times\s*', 'times', string)
-        string=re.sub('\s*{\s*', '{', string)
+        string=re.sub('\s*{\s*', '{ ', string)
         string=re.sub('\s*}', '}', string)
         tokens=string.split()
         l=len(tokens)
@@ -16,12 +16,35 @@ class rithmic_pattern:
         self.values=[]
         self.pre=[]
         self.post=[]
+        self.tie=[]
+        self.rest=[]
+        tresillo=''
         while i<l:
-            self.values.append(tokens[i])
+            if tokens[i] == '~':
+               self.tie[n-1]+='~' + tokens[i+1]
+               i=i+1
+            elif tokens[i][0] == 'r':
+                if n==0:
+                    tresillo+=tokens[i]+' '
+                else:
+                    self.rest[n-1]+= tokens[i]
+            elif tokens[i][-1]=='{':
+                tresillo+=tokens[i]
+            else:
+                r=re.match(r"(\d*\.*)(.*)", tokens[i])
+                numero=r.group(1)
+                if numero == '.':
+                    numero=''
+                scripts=r.group(2)
+                self.pre.append(tresillo)
+                self.values.append(numero)
+                self.post.append(scripts)
+                self.tie.append('')
+                self.rest.append('')
+                tresillo=''
+                n=n+1
             i=i+1
-            n=n+1
         self.l=n
-
         return
 
 class melodic_pattern:
@@ -59,10 +82,25 @@ class escala:
         b=bowing.l
         s=''
         for i in range(m):
+            if i%r==0:
+                s+='\n    '
+            s+=rithm.pre[i%r]
             s+=melody.notas[i]
             s+=rithm.values[i%r]
             s+=bowing.bowings[i%b]
+            s+=rithm.post[i%r]
             s+=' '
+            if rithm.tie[i%r]:
+                ties=rithm.tie[i%r].split('~')
+                ties=ties[1:]
+                print (ties)
+                for j in ties:
+                    s+='~ '
+                    s+=melody.notas[i]
+                    s+=j
+                s+=' '
+            if rithm.rest[i%r]:
+                s+=rithm.rest[i%r]
         self.lilypond_string=s
 
         return
@@ -74,17 +112,18 @@ class escala:
         s ='\\score{\n'
         s+="  \\new Staff \\relative c'{\n"
         s+="    \\key " + self.key_signature + " \\major\n"
-        s+="    \\time " + self.time_signature + "\n"
+        s+="    \\time " + self.time_signature
         s+="    " + self.lilypond_string + "\n"
+        s+='    \\bar "||"'
         s+="  }\n}\n"
         print(s)
 
 
 
-ritmo=rithmic_pattern(r"\times 2/3 { 8 8 8 }")
-melodia=melodic_pattern("c d e f g a b c")
-arcos=bowing_pattern(r"(\(\downbow . \) -.)")
-digitacion=fingering_pattern("1.......")
+ritmo=rithmic_pattern(r"r4 \times 2/3{r8 8 .}", "2/4")
+melodia=melodic_pattern("c d e f g a b c d e f g")
+arcos=bowing_pattern(r".")
+digitacion=fingering_pattern("1...........")
 
 a=escala(melodia, digitacion, arcos, ritmo)
 
